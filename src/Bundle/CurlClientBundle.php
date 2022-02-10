d1ef46c55d564b11c76050f97890943cf3385da0
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2022-02-10 12:39:50 +0200

    Introduce compatibility with Symfony 6

--- src/Bundle/CurlClientBundle.php
+++ src/Bundle/CurlClientBundle.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace Http\Client\Curl\Symfony\Bundle;
 
 use Http\Client\Curl\Symfony\Resources\DependencyInjection\CurlClientExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CurlClientBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ExtensionInterface
     {
         return new CurlClientExtension();
     }
